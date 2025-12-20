19159dab9cee225273ccbb83be28fcdf8b300be9
Your Name <you@example.com>
2025-12-20 14:12:58 +0500

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+  plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 1000, 
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("framer-motion")) {
-              return "framer-motion";
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) {
+              return 'framer-motion';
             }
-            if (id.includes("lucide-react")) {
-              return "lucide-react";
+            if (id.includes('lucide-react')) {
+              return 'lucide-react';
             }
-            return "vendor";
+            return 'vendor';
           }
         },
       },
     },
   },
-});
+})
